77d902de2c3a4cb4f28156fbd36118995ea1fdbd
Huit-8 <eitoao88@gmail.com>
2023-07-02 16:29:30 +0900

    second commit

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,59 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import kotlin.jvm.internal.Intrinsics.Kotlin
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var weatherApi:WeatherApiService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpWeatherApi()
+
+
+
+        lifecycleScope.launch{
+            kotlin.runCatching {
+                weatherApi.getCityWeather("130010")
+            }.onSuccess{
+                binding.cityTextView.text = it.title
+            }.onFailure {
+                Log.d("error",it.message.toString())
+            }
+        }
+
+
+    }
+
+    fun setUpWeatherApi(){
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val httpLogging = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val httpClientBuilder = OkHttpClient.Builder().addInterceptor(httpLogging).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://weather.tsukumijima.net/api/forecast/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(httpClientBuilder)
+            .build()
+
+        weatherApi = retrofit.create(WeatherApiService::class.java)
     }
 }
